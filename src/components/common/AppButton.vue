f121d880e1360b93af38aadbfef5d45a348d8bd8
DDeenis <ddeenis46@gmail.com>
2022-01-07 16:24:27 +0200

    fix disabled styles

--- src/components/common/AppButton.vue
+++ src/components/common/AppButton.vue
@@ -1,8 +1,13 @@
 <template>
   <button
     :disabled="disabled"
-    :class="{ 'bg-gray-300': disabled, 'hover:bg-gray-400': disabled }"
-    class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-500 hover:bg-gray-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    :class="{
+      'bg-gray-300': disabled,
+      'hover:bg-gray-400': disabled,
+      'bg-gray-500': !disabled,
+      'hover:bg-gray-600': !disabled,
+    }"
+    class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
   >
     <slot />
   </button>
